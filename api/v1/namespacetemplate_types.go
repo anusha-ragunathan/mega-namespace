d13596ad3288de32345e70ab8c93a822516dcc5b
Anusha Ragunathan <anusha.ragunathan@docker.com>
2020-03-08 16:25:34 -0700

    Add NamespaceTemplateSpec types
    Added types for NamespaceTemplateSpec. This required:
    - adding go mod dependencies for k8s.io/api/core
    - updating the sample yaml object for namespacespec
    - auto updating of the CRD. A very nice surprise side effect of
    using kubebuilder is that, we get automatic client validation schema for
    the CRD using openAPI v3 schema.
    
    Signed-off-by: Anusha Ragunathan <anusha.ragunathan@docker.com>

--- api/v1/namespacetemplate_types.go
+++ api/v1/namespacetemplate_types.go
@@ -17,19 +17,49 @@ limitations under the License.
 package v1
 
 import (
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
+// ExecAction is inspired from api_v1's ExecAction which executes inside the container via
+// the kubelet. Here, we execute on the master.
+type ExecAction struct {
+	// Command is the command line to execute on the host, the working directory for the
+	// command  is root ('/') in the host's filesystem. The command is simply exec'd, it is
+	// not run inside a shell, so traditional shell instructions ('|', etc) won't work. To use
+	// a shell, you need to explicitly call out to that shell.
+	// Exit status of 0 is treated as live/healthy and non-zero is unhealthy.
+	// +optional
+	Command []string `json:"command,omitempty"`
+}
+
+// This struct contains a static list of k8s resources that can be provisioned by the controller.
+// To implement a dynamic list, we need to use the generic runtime.Object, the dynamic
+// k8s client and receive into a unstructured.unstructured object. However, having static types
+// is pretty useful for other fields of the NamespaceTemplate.
+// Explore the dynamic client later.
+// https://stackoverflow.com/questions/53341727/how-to-submit-generic-runtime-object-to-kubernetes-api-using-client-go
+// Another drawback is only one instance can be provisioned per resource type.
+// XXX: Fix this later.
+type AdditionalResources struct {
+	PodSpec        v1.PodSpec    `json:"podspec"`
+	SecretSpec     v1.Secret     `json:"secretspec"`
+	LimitRangeSpec v1.LimitRange `json:"limitrangespec"`
+}
+
 // NamespaceTemplateSpec defines the desired state of NamespaceTemplate
 type NamespaceTemplateSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// XXX: could be omitempty. which of these is optional?
+	Options map[string]string `json:"options,omitempty"`
 
-	// Foo is an example field of NamespaceTemplate. Edit NamespaceTemplate_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	PreCreateHook ExecAction `json:"precreatehook,omitempty"`
+
+	PostCreateHook ExecAction `json:"postcreatehook,omitempty"`
+
+	AddResources AdditionalResources `json:"addresources,omitempty"`
 }
 
 // NamespaceTemplateStatus defines the observed state of NamespaceTemplate
